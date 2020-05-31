d95d02ebcf3b34472a0622f5d064a8d1c0a242ce
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-06-01 01:07:51 +0300

    FODCircleView: Improve position update
    1. Do not apply dreaming offset when showing pressed icon;
    2. Update position when showing pressed icon;
    3. Update position when exiting dream.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I56edb6c5d38c51b85521444109904254aafe7063
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -444,6 +444,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener,Tu
 
         setImageDrawable(null);
         updateIconDim();
+        updatePosition();
         invalidate();
     }
 
@@ -552,7 +553,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener,Tu
         mPressedParams.x = mParams.x = x;
         mPressedParams.y = mParams.y = y;
 
-        if (mIsDreaming) {
+        if (mIsDreaming && !mIsCircleShowing) {
             mParams.y += mDreamingOffsetY;
         }
 
